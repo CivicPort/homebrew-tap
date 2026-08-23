3abaa193988de68a90da35c5e1cfc217d88dc828
Evan <ehaston@darkmatterit.io>
2026-08-23 15:38:55 -0400

    Update commserv.rb

--- Casks/commserv.rb
+++ Casks/commserv.rb
@@ -2,7 +2,7 @@ cask "ember-messenger" do
   version "0.9.0"
   sha256 "2d0c2a6a3361a515f3a5ba0e62450b3868117d78bd64d6a65106b29aff249c48"
 
-  url "https://github.com/CloudFlowAutomation/Ember/releases/download/v#{version}/CommSecure-darwin-universal-#{version}.zip"
+  url "https://github.com/CivicPort/Ember/releases/download/v#{version}/CommSecure-darwin-universal-#{version}.zip"
   name "ember-messenger"
   desc "End-to-end encrypted group chat client with perfect forward secrecy"
   homepage "https://github.com/CivicPort/Ember"
