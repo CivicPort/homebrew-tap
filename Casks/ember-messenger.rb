af768e141bdc5c8a51840eeacc07b2738165ab25
Evan <ehaston@darkmatterit.io>
2026-08-23 15:46:18 -0400

    Update ember-messenger.rb

--- Casks/ember-messenger.rb
+++ Casks/ember-messenger.rb
@@ -1,20 +1,21 @@
+
 cask "ember-messenger" do
   version "0.9.0"
   sha256 "2d0c2a6a3361a515f3a5ba0e62450b3868117d78bd64d6a65106b29aff249c48"
-
+ 
   url "https://github.com/CivicPort/Ember/releases/download/v#{version}/CommSecure-darwin-universal-#{version}.zip"
   name "ember-messenger"
   desc "End-to-end encrypted group chat client with perfect forward secrecy"
   homepage "https://github.com/CivicPort/Ember"
-
+ 
   livecheck do
     url :url
   end
-
+ 
   depends_on macos: :monterey
-
+ 
   app "Ember.app"
-
+ 
   zap trash: [
     "~/Library/Application Support/Ember",
     "~/Library/Caches/io.darkmatterit.ember",
