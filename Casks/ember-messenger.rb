b1eb9d54cb330859d6f2869689e97985a67716e0
Evan <ehaston@darkmatterit.io>
2026-08-23 23:10:32 -0400

    Update ember-messenger.rb

--- Casks/ember-messenger.rb
+++ Casks/ember-messenger.rb
@@ -1,6 +1,6 @@
 
 cask "ember-messenger" do
-  version "0.10.1"
+  version "0.10.0"
   sha256 "4244986debe4bd0e606dca04bd9be0a4ce9d152d50612dd8175348e501b8d858"
  
   url "https://github.com/CivicPort/Ember/releases/download/v#{version}/Ember-darwin-universal-#{version}.zip" 
